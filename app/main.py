996c6548f39abe40675dcd190f9ad75eb3d6b052
jhonux <jhonatah197@gmail.com>
2025-11-13 21:51:22 -0300

    fix: corrige formatação do main.py para conformidade com flake8

--- app/main.py
+++ app/main.py
@@ -10,11 +10,6 @@ def verificar_aprovacao(valores):
 
 
 if __name__ == "__main__":
-
-    prompt = "Digite as notas separadas por espaço: "
-    entrada_usuario = input(prompt)
-
-    notas = [float(x) for x in entrada_usuario.split()]
-
+    entrada = input("Digite as notas separadas por espaço: ")
+    notas = [float(x) for x in entrada.split()]
     print(verificar_aprovacao(notas))
-    
