59a94fc5d150c7230901b2ff3c5d0b885e31fcf3
jhonux <jhonatah197@gmail.com>
2025-11-15 17:35:37 -0300

    feat: Adicionando infra Terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   features {}
+  subscription_id = "d1d02f10-2705-4dcc-9caa-c17bcd92e98a"
 }
 
 resource "azurerm_resource_group" "rg_devops" {
@@ -8,7 +9,7 @@ resource "azurerm_resource_group" "rg_devops" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "storagedevopsequipe"
+  name                     = "storagedevopsjonatas04"
   resource_group_name      = azurerm_resource_group.rg_devops.name
   location                 = azurerm_resource_group.rg_devops.location
   account_tier             = "Standard"
