02293c5a6131d0873d79c9d6ed323afb81a2042e
JoaoSantos407 <joao.silva40@outlook.com.br>
2025-11-15 23:57:00 -0300

    Adicionando projeto terraform

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
 provider "azurerm" {
   features {}
+  subscription_id = "70c39920-137e-46a1-82dc-e2a69f1cf829"
 }
 
 resource "azurerm_resource_group" "rg_devops" {
   name     = "rg-devops-equipe"
-  location = "Brazil South"
+  location = "northcentralus"
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "storagedevopsequipe"
+  name                     = "storagedevopjoaovictor3"
   resource_group_name      = azurerm_resource_group.rg_devops.name
   location                 = azurerm_resource_group.rg_devops.location
   account_tier             = "Standard"
