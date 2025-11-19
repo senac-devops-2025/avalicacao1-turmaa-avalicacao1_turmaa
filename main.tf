45bd566bdaaf2afee365b6bf891120b7a50e321c
Rafael Goulart <rafaelxms@hotmail.com>
2025-11-19 19:42:20 -0300

    feat: Adiciona Infra Terraform

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
+
 provider "azurerm" {
+  subscription_id = "e37f0453-2f96-4df5-a817-844232ac0687"
   features {}
 }
 
 resource "azurerm_resource_group" "rg_devops" {
-  name     = "rg-devops-equipe"
+  name     = "rg-devops-equipe-grupo-1-rafael-goulart"
   location = "Brazil South"
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "storagedevopsequipe"
+  name                     = "storagedevopsequipe1rafa"
   resource_group_name      = azurerm_resource_group.rg_devops.name
   location                 = azurerm_resource_group.rg_devops.location
   account_tier             = "Standard"
